f541471d68745881b9a81551246ef183d51f7d25
cas <595972434@qq.com>
2019-04-18 11:33:55 +0800

    Practice-6 OK 子类继承和方法重写，添加Theacher类，使用super调用父类方法，添加类成员变量初始化

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,23 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    public int klass=0;
+    public Teacher(String tom, int i,int j) {
+        super(tom,i);
+        klass=j;
+    }
+    public Teacher(String tom, int i) {
+        super(tom,i);
+    }
+    public int getKlass() {
+        return klass;
+    }
+    public String introduce() {
+        if(klass==0){
+            return super.introduce()+" I am a Teacher. I teach No Class.";
+        }
+        else{
+            return super.introduce()+" I am a Teacher. I teach Class "+this.klass+".";
+        }
+    }
 }
